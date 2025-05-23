46e3c7b71af8befd27f5287651251c0305208300
m1shshka <mishanosikovyet@gmail.com>
2025-05-23 15:44:09 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-```python
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from flask import Flask, request
@@ -59,4 +58,3 @@ def webhook():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-```
